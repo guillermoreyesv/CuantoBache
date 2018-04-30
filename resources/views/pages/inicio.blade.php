047a74c90c3f655ab620a9f34135f02dbc64de47
Memo <guillermo_hard@hotmail.com>
2018-04-30 00:50:11 -0500

    Se modifico la interfaz de inicio# Please enter the commit message for your changes. Lines starting

--- resources/views/pages/inicio.blade.php
+++ resources/views/pages/inicio.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
-    <div class="" id='map'></div>
+    <div class="align-middle text-center" id='map'>CARGANDO ...</div>
     <script>
     	$(document).ready(function () {
 
@@ -9,38 +9,74 @@
 		    });
 
 		});
-		function initMap() {
-	      	var coords = {}; 
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(
-				function (position){
-					coords =  {
-						lng: position.coords.longitude,
-						lat: position.coords.latitude
-					};
-					setMapa(coords);  //pasamos las coordenadas al metodo para crear el mapa
-				},
-				function(error){
-					coords =  {
-						lng: -99.0737071,
-						lat: 19.4541067
-					};
-					setMapa(coords);
-				});
+
+		function centrarPocision(myLatlng){
+			var map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 15,
+				center: myLatlng
+			});
+			for(i=0;i<10;i++){
+				
+				myLatlng={lat: myLatlng.lat+(i/10000), lng: myLatlng.lng+(i/10000)}
+				/* MOSTRANDO UN MARCADOR CON UNA LONGITUD Y LATITUD DEFINIDA*/
+				var marker = new google.maps.Marker({
+				position: myLatlng,
+				map: map,
+				title: 'Bache :('
+				});	
 			}
-			else {
-			  alert('Geolocation is not supported for this Browser/OS.');
+			
+			map.addListener('click', function(position) {
+				// 3 seconds after the center of the map has changed, pan back to the
+				// marker.
+				placeMarkerAndPanTo(position.latLng, map);
+				/*window.setTimeout(function() {
+				  map.panTo(marker.getPosition());
+				}, 3000);*/
+			});
+
+			marker.addListener('click', function() {
+				map.setZoom(8);
+				map.setCenter(marker.getPosition());
+			});
+		}
+
+		function initMap() {
+			//Latitud predeterminada
+			var myLatlng = {lat: 19.4167476, lng: -99.1681686};
+			//Comprobamos que el navegador soporta geolocalizacion
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(function(position) {
+					//alert("Exito");
+					//establecemeos nuevas coordenadas
+					myLatlng={lat: position.coords.latitude, lng: position.coords.longitude};
+					//Centramos el mapa
+					centrarPocision(myLatlng);
+				},function(error){
+					//alert("Error");
+					//Definimos variables
+				    myLatlng = {lat: 19.4167476, lng: -99.1681686};
+				    //Centramos el mapa
+				    centrarPocision(myLatlng);
+				},{
+					maximumAge: 75000,
+					timeout: 1000
+				});
+			} else {
+			    //alert("Geolocation is not supported by this browser.");
+			    //Definimos variables
+			    myLatlng = {lat: 19.4167476, lng: -99.1681686};
+			    //Centramos el mapa
+			    centrarPocision(myLatlng);
 			}
 		}
-		function setMapa(coords){
-	        var map = new google.maps.Map(document.getElementById('map'),{
-		        zoom: 15,
-		        center:new google.maps.LatLng(coords.lat,coords.lng),
-			});
-	        var marker = new google.maps.Marker({
-	          position: coords,
-	          map: map
-	        });
+
+		function placeMarkerAndPanTo(latLng, map) {
+		  var marker = new google.maps.Marker({
+		    position: latLng,
+		    map: map
+		  });
+		  map.panTo(latLng);
 		}
     </script>
     <script async defer
